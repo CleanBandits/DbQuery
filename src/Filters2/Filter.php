b572b8abec5cd3da7b8d37ab78c698ac406eb4fe
jskesters <juris.skesters@gmail.com>
2020-09-12 14:47:49 +0300

    #3 Casing fix

--- src/Filters2/Filter.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace CleanBandits\DbQuery\Filters2;
-
-interface Filter
-{
-    public function sql(): string;
-}
--- src/Order2/DbOrderDirection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CleanBandits\DbQuery\Order2;
-
-class DbOrderDirection
-{
-    const ASC = 'ASC';
-    const DESC = 'DESC';
-}
